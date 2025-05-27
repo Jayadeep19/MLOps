7770404c286afdb198828d0562f47775231f81b0
Jayadeep <103530867+Jayadeep19@users.noreply.github.com>
2025-05-27 21:53:49 +0000

    homework-02

---
+++ 02-experiment_tracking/train.py
@@ -0,0 +1,54 @@
+import os
+import pickle
+import click
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import root_mean_squared_error
+
+
+import mlflow
+
+
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("nyc-taxi-experiment-homework02")
+mlflow.sklearn.autolog()
+
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+@click.command()
+@click.option(
+    "--data_path",
+    default="./output",
+    help="Location where the processed NYC taxi trip data was saved"
+)
+def run_train(data_path: str):
+
+    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+    with mlflow.start_run():
+
+        mlflow.set_tag("developer","jayadeep")
+        mlflow.log_param("train-data-path", "./green_tripdata_2021-01.csv")
+        mlflow.log_param("valid-data-path", "./green_tripdata_2021-02.csv")
+        mlflow.log_param("test-data-path", "./green_tripdata_2021-03.csv")
+        max_depth = 10
+        random_state = 0
+        
+
+        rf = RandomForestRegressor(max_depth=max_depth, random_state=random_state)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = root_mean_squared_error(y_val, y_pred)
+        mlflow.log_metric("rmse", rmse)
+
+    min_split = rf.get_params()['min_samples_split']
+    print("min_samples_split:", min_split)
+
+if __name__ == '__main__':
+    run_train()
